ab68387785bb548ce580b6f736c9a24eddd44de6
sctwomey <sctwomey@gmail.com>
2023-09-19 11:11:53 -0700

    Update html and css files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
-let requestUrl = 'https://api.openbrewerydb.org/v1/breweries';
+// let requestUrl = 'https://api.openbrewerydb.org/v1/breweries';
 
-fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    });
+// fetch(requestUrl)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     });
+
+let searchFormEl = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+
+    let searchInputVal = document.querySelector('#search-input').value;
+    let formatInputVal = document.querySelector('#format-input').value;
+
+    if (!searchInputVal) {
+        console.error('You need a search input value!');
+        return;
+    }
+
+    // let queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+    // location.assign(queryString);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);

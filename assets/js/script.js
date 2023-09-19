e35065bfe085b255fe4c9ba742f154e4fa09329e
sctwomey <sctwomey@gmail.com>
2023-09-19 11:26:14 -0700

    Update script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,3 @@
 //     .then(function (data) {
 //         console.log(data);
 //     });
-
-let searchFormEl = document.querySelector('#search-form');
-
-function handleSearchFormSubmit(event) {
-    event.preventDefault();
-
-    let searchInputVal = document.querySelector('#search-input').value;
-    let formatInputVal = document.querySelector('#format-input').value;
-
-    if (!searchInputVal) {
-        console.error('You need a search input value!');
-        return;
-    }
-
-    // let queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-    // location.assign(queryString);
-}
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);

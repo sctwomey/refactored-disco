0c878e05afd88b08ffd11b36a0aae9860083fa1a
Mathew Poppell <mpoppell1002@gmail.com>
2023-09-19 15:54:14 -0400

    updated html/updated initial JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 let requestUrl = 'https://api.openbrewerydb.org/v1/breweries';
 var searchButton = document.querySelector()
+var formatInputVal = document.querySelector ("#project-type-input")
+
 fetch(requestUrl)
     .then(function (response) {
         return response.json();
@@ -7,6 +9,11 @@ fetch(requestUrl)
     .then(function (data) {
         console.log(data);
     });
+
+
+    var querystring = './search-results.html?q=' + formatInputVal + '&by_name='
+    location.assign(querystring)
+
 
 
 

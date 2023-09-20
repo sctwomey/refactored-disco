9bd9b1bfb54ebb8adc1b4ebdb34294f8313cd8c9
sctwomey <sctwomey@gmail.com>
2023-09-20 10:56:08 -0700

    Add get function and create elements in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,57 @@
-let requestUrl = 'https://api.openbrewerydb.org/v1/breweries';
 
-fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
+async function getBreweries() {
+    try {
+        const response = await fetch(
+            'https://api.openbrewerydb.org/v1/breweries',
+            {
+                method: 'GET',
+            },
+        );
+
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        };
+
+        const data = await response.json();
+
+        return data;
+
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+getBreweries().then(data => {
+    console.log(data);
+
+    const ol = document.createElement('ol');
+
+    data.forEach(brewery => {
+        const li = document.createElement('li');
+        li.innerHTML = `${brewery.name}`;
+
+        li.style.fontSize = '22px';
+
+        const ul = document.createElement('ul');
+
+        const city = document.createElement('li');
+        city.innerHTML = `City: ${brewery.city}`;
+
+        const state = document.createElement('li');
+        state.innerHTML = `State: ${brewery.state}`;
+
+        const country = document.createElement('li');
+        country.innerHTML = `Country: ${brewery.country}`;
+
+        ul.append(...[city, state, country]);
+
+        li.appendChild(ul);
+
+        ol.appendChild(li);
+
     });
 
+    const container = document.getElementById('container');
+    container.appendChild(ol);
 
-// GET https://api.openbrewerydb.org/v1/breweries?by_postal=44107&per_page=3
+});

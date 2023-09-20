aeab054866e7bf97554a36b7af8b71c0819a53f7
Mathew Poppell <mpoppell1002@gmail.com>
2023-09-20 10:57:58 -0400

    updated html labels for ease of use. added onto JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+//function getApi() {
 let requestUrl = 'https://api.openbrewerydb.org/v1/breweries';
-
+var searchButton = document.querySelector("button")
+var formatInputVal = document.querySelector ("#project-type-input")
 
 fetch(requestUrl)
     .then(function (response) {
@@ -7,6 +9,18 @@ fetch(requestUrl)
     })
     .then(function (data) {
         console.log(data);
+
+        var brewery = (data)
+
+        for (var i= 0; i < data.length; i++) {
+        var breweryName= brewery[i].name;
+        var breweryCity= brewery[i].city;
+        var breweryState= brewery[i].state;
+        var breweryCountry= brewery[i].country;
+        var breweryPhone= brewery[i].phone;
+        }
+
+
     });
 
 
